ee0cdc97e4e312fe1913f6b1e01f13b4890600ee
max09w0 <max09w0@gmail.com>
2026-08-02 01:46:50 +0700

    Add model products(Category, Product), others(Banner, Post)

--- others/models.py
+++ others/models.py
@@ -5,3 +5,20 @@ from django.db import models
 # Model Banner (làm giống thầy) 
 # Model Post (làm giống thầy)
 # Trên web thầy có để ví dụ
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='banners/')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- products/models.py
+++ products/models.py
@@ -20,3 +20,29 @@ from django.db import models
     # created_at(tự động điền)
     # )
 # Status của Product nếu khó quá thì bỏ qua nha
+
+#chưa có STATUS
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='products/')
+    is_available = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
