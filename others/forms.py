8f970b73698d21a0a380abdcc8253c6d2a1cd576
tuantu1711 <duongtuantu171110@gmail.com>
2026-08-08 23:36:31 +0700

    formsnvt2

--- others/forms.py
+++ others/forms.py
@@ -5,3 +5,12 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+    def clean_content(self):
+        data = self.cleaned_data["content"]
+
+        forbidden_words = ['ăn cắp', 'tuồn kho', 'lừa đảo', 'hack', 'scam', 'fake', 'hàng giả']
+
+        if any(word in data for word in forbidden_words):
+            raise forms.ValidationError("Vui lòng sử dụng ngôn từ lịch sự!")
+
+        return data
