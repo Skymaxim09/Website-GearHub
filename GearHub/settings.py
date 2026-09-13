5c958aa96fe1b5294e0a585bfe5044fa7f54f36b
max09w0 <max09w0@gmail.com>
2026-09-13 14:44:55 +0700

    Feat: Edit setting, urls and Add context_processors for Project

--- GearHub/settings.py
+++ GearHub/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     
     'products',
     'orders',
@@ -67,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
+                'GearHub.context_processors.categories',
             ],
         },
     },
